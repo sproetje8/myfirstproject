2e0c8c41aadae90393d086731f5755b35fd00db1
Evelien Kulikova <evelienfoubert@mail.ru>
2020-03-05 10:45:13 +0300

    feat: Add the functions for the API methods getLangs, detect and translate

--- JS/config.js
+++ JS/config.js
@@ -1,1 +1,4 @@
-const APIkey = "trnsl.1.1.20200303T102859Z.6823171d9619f449.50fd2197c0e0b5befffaf0806fafd924b64f046b";
+const apiKey =
+	'trnsl.1.1.20200303T102859Z.6823171d9619f449.50fd2197c0e0b5befffaf0806fafd924b64f046b';
+const apiAddress = 'https://translate.yandex.net/api/v1.5/tr.json/';
+const apiMethods = ['getLangs', 'detect', 'translate'];
--- JS/myTranslator.js
+++
@@ -1,1 +0,0 @@
-console.log('firing this thing');
